d68f7a7123f466c2cab86fc4bc2e9b9eaa75fa90
mossman252 <joe_mossy@hotmail.com>
2013-02-01 14:16:25 -0500

    Second commit

--- MyFirstProject/src/com/example/myfirstproject/LoginActivity.java
+++ MyFirstProject/src/com/example/myfirstproject/LoginActivity.java
@@ -24,6 +24,8 @@ public class LoginActivity extends Activity {
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
      */
+	int joe;
+	
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello",
             "bar@example.com:world"
